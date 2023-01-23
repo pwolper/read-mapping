61fe6ab0e80153585c16be62ec087f330ce5646f
pwolper <philip.wolper@gmail.com>
2023-01-23 14:03:32 +0100

    modified main.py

--- main.py
+++ main.py
@@ -2,15 +2,33 @@
 
 # Running the read-mapper defined in mapping.py
 #
-
 from mapping import *
+import re
 
 ## Map the Genome and generate summary file and Barplot
 # mapGenome("data/Ecoli_genome.fasta", "data/reads_new.fasta")
 
-Genome = getGenome("data/Ecoli_genome.fasta")
+# genome = getGenome("data/Ecoli_genome.fasta")
+genome = getGenome("data/Ecoli_genome.fasta")[0]
+Gen = getGenome("data/Ecoli_genome.fasta")[1]
+Reads = getReads("data/reads_new.fasta")[0:2]
 
-Reads = getReads("data/reads_new.fasta")
-print(Reads[0:2])
-Comp = compStrand(Reads[0:2])
+
+print(Reads)
+Comp = compStrand(Reads)
 print(Comp)
+
+mapped = {}
+genome = str(genome)
+# Gen = genome[1][0]
+
+# for read in Reads:
+#     posFor = [i.start() for i in re.finditer(read, genome)]
+#     print("Position on forward strand:",posFor)
+#     posRev = [i.start() for i in re.finditer(read, compStrand(genome))]
+#     print("Position on rev strand:", posRev)
+#     posOut = posFor + posRev
+#     print(posOut)
+#     mapped[Gen,read] = posOut
+
+print(mapGenome(genome)))
