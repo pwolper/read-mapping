2a385f2f4b5f9a597c8dd5521b612660696c40a7
pwolper <philip.wolper@gmail.com>
2023-01-26 17:14:42 +0100

    improved mapping.py, csv still broke

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-
-# Running the read-mapper defined in mapping.py
-#
+from time import ctime
 from mapping import *
 
+print("Currently:", time.ctime())
 path = "./output"
 
+
+# Running the read-mapper defined in mapping.py
 genome = getGenome("data/Ecoli_genome.fasta")
-Reads = getReads("data/reads_new.fasta")[0:50]
+Reads = getReads("data/reads_new.fasta")[0:100]
 
-mapped = mapGenome(genome,Reads,path)                             #mapping reads
+mapped = mapGenome(genome,Reads)                             #mapping reads
 
-MapSummary(Reads, mapped, genome, path)
+
+MapSummary(mapped, genome, path)
 Barplot(Reads,mapped, genome, path)
--- mapping.py
+++ mapping.py
@@ -1,9 +1,10 @@
 import time
+import os
+import re                                                       #use re for easier iterate search
+import csv
 from datetime import datetime
 from Bio import SeqIO                                           #load SeqIO from Biopython package
 import matplotlib.pyplot as plt
-import os
-import re                                                       #use re for easier iterate search
 
 def getGenome(file):                                    #return sequences and description from fasta file (input .fasta)                    #returns array of array
     sequences = []                                          #create empty array for for-loop
@@ -12,16 +13,19 @@ def getGenome(file):                                    #return sequences and de
         seq = record.seq
         des = record.description
         sequences.append(str(seq))
+        print(des)
         descriptions.append(des)
     return(sequences, descriptions)
 
 
 def getReads(file):                                     #return reads to map from source file as array (input preferably txt)
+    nucl = ["A","T","C","G"]
     reads = []
     with open(file) as read:
         for line in read.readlines():
-            line = line[:-1]
-            reads.append(line)
+            if any(base in line for base in nucl):
+                line = line[:-1]
+                reads.append(line)
     return (reads)
 
 def compStrand(sequences):                                    #returns the complementary string of entered sequence as string
@@ -39,67 +43,60 @@ def compStrand(sequences):                                    #returns the compl
 
 
 
-def Barplot(gotReads, mappedGenome, gotGenome, path):             #create a bar plot showing occurrence for all reads for each genome; input return from getReads, mapGenome and getGenome
+def Barplot(gotReads, mappedGenome, gotGenome, path = "./"):             #create a bar plot showing occurrence for all reads for each genome; input return from getReads, mapGenome and getGenome
+    if not path =="./":                                              #create output path if not existing
+        if not os.path.exists(path):
+            os.makedirs(path)
+    fileName = str(path)+ "/"+str(datetime.today().strftime('%Y-%m-%d_'))+ str(gotGenome[1][0]) + ".png"
+
     number = []
-    # l = len(gotGenome[1])
-    # for i in range(l):
-    for entry in gotReads:
+    for entry in mappedGenome:
             m = mappedGenome[entry][0] + mappedGenome[entry][1]
             number.append(len(m))
-    fileName = str(path)+ "/"+str(datetime.today().strftime('%Y-%m-%d_'))+ str(gotGenome[1]) + ".png"
     plt.bar(range(len(number)), number)
     plt.xlabel("Read Number in File")
     plt.ylabel("Occurrences in Genome")
     plt.savefig(fileName)
     plt.clf()
-    print("Boxplot created!")
+    print("Boxplot created in",path,"!")
 
-def MapSummary(gotReads, mappedGenome, gotGenome, path):          #create a summary after the genome is mapped in a txt file for each genome; input return from getReads, mapGenome and getGenome
-    summary = {}
-    # l = len(gotGenome[1])
-    # for i in range(l):
-    fileName = str(path)+ "/" +str(datetime.today().strftime('%Y-%m-%d_'))+ str(gotGenome[1]) +".csv"            #create file name for current genome from fasta description
-    for Read in gotReads:
-            summary[Read] = mappedGenome[Read]      #create dictionary for read and positions (array)
-            with open(fileName, "w") as f:                                     #create file
-                f.write("Read; Forward Start 5' -> 3'; Reverse Start 5' -> 3'; Number of Hits \n")
-                for element in summary:
-                    f.write(element)                                        #write read
-                    f.write("; ")
-                    f.write(", ".join(str(x) for x in summary[element][0]))    #write forward positions, separated by ";"
-                    f.write("; ")
-                    f.write(", ".join(str(x) for x in summary[element][1]))    #write backward positions, separated by ";"
-                    f.write("; [")
-                    f.write(str(len(summary[element])))                     #write number of found positions
-                    f.write("]")
-                    f.write("\n")
-    print("Successfully wrote summary file!")
-
-def mapGenome(genomes, Reads, path ="./"):                                      #return dictionary with genome description and read as key and starting positions in genome as value (returns list of list
-    start = time.time()
+def MapSummary(mappedGenome, gotGenome, path = "./"):          #create a summary after the genome is mapped in a txt file for each genome; input return from getReads, mapGenome and getGenome
     if not path =="./":                                              #create output path if not existing
         if not os.path.exists(path):
             os.makedirs(path)
-    # Genomes = getGenome(G_file)                                     #store sequences and description from fasta files (input .fasta)
-    # print("Finished evaluating 'getGenomes'!")
-    # Reads = getReads(R_file)                                        #store reads to map from source file as array (input preferably txt)
-    # print("Finished evaluating 'getReads'!")
+    fileName = str(path)+ "/" +str(datetime.today().strftime('%Y-%m-%d_'))+ str(gotGenome[1][0]) +".csv"            #create file name for current genome from fasta description
+
+    with open(fileName, "w", newline = "") as f:                                     #create file
+        f = csv.writer(f)
+        header = ["Read; Sense; Antisense; Number of sense hits; Number of antisense hits; Number of total hits"]
+        f.writerow(header)
+        for read in mappedGenome:
+            read = read
+            sense = ",".join(str(x) for x in mappedGenome[read][0])
+            anti = ",".join(str(x) for x in mappedGenome[read][1])
+            n_sense = len(mappedGenome[read][0])                     #write number of found positions
+            n_anti = len(mappedGenome[read][1])
+            n_total = n_sense + n_anti
+            cols = [read,sense,anti,n_sense,n_anti,n_total]
+            entry = ";".join(str(x) for x in cols)
+            f.writerow(entry)
+
+    print("Successfully wrote summary file to",path,"!")
+
+def mapGenome(genomes, Reads):                                      #return dictionary with genome description and read as key and starting positions in genome as value (returns list of list
+    start = time.time()
+    print("mapping reads to genome...")
+
     mapped = {}                                                     #create dictionary for output
-    # g = 0                                                           #
-    # for genome in genomes[0]:
-    Gen = genomes[1]
-    #    g = +1
     genome = str(genomes[0])
-      #  compgenome = compStrand(genome)
     for read in Reads:
           posFor = [i.start() for i in re.finditer(read, genome)]
           posRev = [i.start() for i in re.finditer(read, compStrand(genome))]
           posOut = [posFor, posRev]
           mapped[read] = posOut
-    # Barplot(Reads,mapped, Genomes, path)
-    # MapSummary(Reads,mapped, Genomes, path)
+
     end = time.time()
-    print("Finished mapping reads in",round(end-start),"s!")
+    print("Finished mapping",len(Reads), "reads in",round(end-start),"s!")
     return(mapped)
     
     
